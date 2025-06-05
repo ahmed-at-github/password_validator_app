e2e3a04ed0f177bb3ceaecbcdbf016ab29fb74aa
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-05 21:45:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import PassValidComp from './components/PassValidComp'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hello World! Iam App</h1>
+     <PassValidComp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PassValidComp.jsx
@@ -0,0 +1,7 @@
+
+
+export default function PassValidComp() {
+    return (
+        <div>Hello World!, Iam PassValidComp</div>
+    )
+}
