d8bbbbf07e1d982d1377660ef09c432b016c0b3d
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-09 14:06:49 +0600

    Logic + UI coupled

--- src/components/PassValidComp.jsx
+++ src/components/PassValidComp.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
+import validator from "validator";
 
 
 export default function PassValidComp() {
+    //STATE Logic
+    const [errorMsg, setErrorMsg] = useState("");
+    const [style, setStyle] = useState("danger");
+
+
+    //EVENT Handler 
+    const validate = (pass) => {
+        if (validator.isStrongPassword(pass, {
+            minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
+        })) {
+            setErrorMsg("Is Strong Password")
+            setStyle("success")
+        }
+        else {
+            setErrorMsg("Is not Strong Password")
+            setStyle("danger")
+        }
+        console.log(style);
+        
+    }
+
     return (
         <>
             <div>
                 <div className="bg-muted flex column align-center ">
                     <h2 className="larger">Password Strength</h2>
-                    <pre className="font-mono">
-                        <span>Enter Password:</span> <input className="bd-default" type="text" onChange={0} />
+                    <pre className="">
+                        <span>Enter Password:</span> <input className="bd-default" type="text" onChange={(e) => validate(e.target.value)} />
                     </pre>
 
-                    { /*0 ? null : */    (<span>{ }</span>)}
+                    {errorMsg === "" ? null : (<p className={style}>{errorMsg}</p>)}
                 </div>
             </div>
         </>
